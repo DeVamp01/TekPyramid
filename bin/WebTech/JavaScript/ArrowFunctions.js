1551925b734d8339e50d4c6105d10209b1c5e3f1
Kiruthi de Vamp <dev.kiruthi@gmail.com>
2025-07-05 18:18:11 +0530

    "day1 JavaScript"

---
+++ bin/WebTech/JavaScript/ArrowFunctions.js
@@ -0,0 +1,19 @@
+var func =(a,b) => a+b;
+console.log(func(10,20))
+
+
+
+// Basic syntax
+let myFunction = (param1, param2) => expression;
+
+// With a single parameter (parentheses can be omitted)
+let singleParamFunction = param => expression;
+
+// With no parameters
+let noParamFunction = () => expression;
+
+// With a block body (requires explicit return)
+let blockBodyFunction = (param1, param2) => {
+  // multiple statements
+  return value;
+};
